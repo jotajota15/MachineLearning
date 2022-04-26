b00793c6be09091850ab74ee29b7b6bd10b390b1
jotajota15 <jjvalver15@gmail.com>
2022-04-26 06:35:09 -0600

    It's made changes to get the class myPCA and to get again the previous results in the Codigo notebook

--- PCA/ML.py
+++ PCA/ML.py
@@ -4,14 +4,14 @@ import numpy as np
 class myPCA:
 
     def __init__(self,matrixArray):
-        self.myMatrix = self.convertMatrix(matrixArray)
+        self.matrixArray = self.convertMatrix(matrixArray)
         self.correlationMatrix = self.correlation()
+        self.eigVal, self.eigVec = self.eigen()
+        self.C = np.matmul(np.transpose(self.matrixArray),self.eigVec)
+        self.inertia = self.getInertia()
         
 
-        self.eigVal, self.eigVec = np.linalg.eigh(self.correlationMatrix)
-        self.eigVal = np.absolute(self.eigVal)
-        sortIndex = np.argsort(self.eigVal)
-        sortIndex
+
 
     def convertMatrix(self,matrixArray):
         '''
@@ -19,8 +19,6 @@ class myPCA:
         '''
         mean = [np.mean(value) for value in matrixArray]
         desviacion = [np.std(value) for value in matrixArray]
-
-
         for iy, ix in np.ndindex(matrixArray.shape):
             matrixArray[iy, ix] = (matrixArray[iy, ix] - mean[iy]) /  desviacion[iy]
         return matrixArray
@@ -33,5 +31,17 @@ class myPCA:
         correlation = matrix/self.matrixArray.shape[0]
         return correlation
 
-
-        
+    def eigen(self):
+        eigVal, eigVec = np.linalg.eigh(self.correlationMatrix)
+        eigVal = np.absolute(eigVal)
+        sortIndex = np.argsort(eigVal)[::-1]
+        eigVal = eigVal[sortIndex]
+        eigVec = np.transpose(eigVec)
+        eigVec = eigVec[sortIndex]
+        eigVec = np.transpose(eigVec)
+        print(sortIndex)
+        return eigVal,eigVec
+    
+    def getInertia(self):
+        totalValues = self.correlationMatrix.shape[0]
+        return [value/totalValues for value in self.eigVal]
