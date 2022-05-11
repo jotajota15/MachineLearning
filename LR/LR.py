380fa5d1612739273fcf53866f381977ed73ff70
jotajota15 <jjvalver15@gmail.com>
2022-05-10 21:32:07 -0600

    Issue #9 completed

---
+++ LR/LR.py
@@ -0,0 +1,10 @@
+
+class myPCA:
+
+    def __init__(self):
+        return
+
+    def fit(self, x, y, max_epochs=100, threshold=0.01, learning_rate=0.001, momentum=0, decay=0, error = 'mse', regularization='none', lambdaV=0):
+        pass
+    def  predict(self, x):
+        pass
