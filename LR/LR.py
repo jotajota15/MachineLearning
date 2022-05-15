174d13201f6e9a6f6fb5db98b5a3e118fac15b86
jotajota15 <jjvalver15@gmail.com>
2022-05-14 23:30:18 -0600

    Solves minor issues to had and MVP, however the solutions are not what was expected.

--- LR/LR.py
+++ LR/LR.py
@@ -5,18 +5,16 @@ import numpy as np
 import sys
 import itertools
 def MSE(y_true, y_predict):
-    return (y_true - y_predict).pow(2).mean()
+    return np.mean(np.power((y_true - y_predict),2))
 
 def MAE(y_true, y_predict): 
-    return ((y_true - y_predict)).abs().mean()
+    return np.mean(np.absolute(y_true - y_predict))
 
 def GDMAE(x,y_true,y_predict):
-    return (np.sign(y_true-y_predict).mul(x)).mean()
+    return (np.sign(y_true-y_predict).dot(x))/y_true.size
     
 def GDMSE(x,y_true,y_predict):
-    # TODO Make every function step by step to see if the results are what you want it
-
-    return (2*(y_true-y_predict)).ravel().transpose().dot(x).transpose()/y_true.size
+    return ((2*(y_true-y_predict)).dot(x))/y_true.size
 
 # R2 = ∑ (yi - f(xi))2 /  ∑ (yi - ∑(f(xj))/n )2
 # R2 = ∑ (yi - f(xi))2 /  ∑ (yi - f(x)mean )2
@@ -27,7 +25,7 @@ def score(y_true, y_predict):
 REGULARIZATION = ["l1", "lasso","l2" "ridge", "elastic-net",None]
 
 def L1(C,lamda):
-    return C.abs().sum()*lamda
+    return np.sum(np.absolute(C))*lamda
 
 def L2(C,lamda):
     return np.power(C,2).sum()*lamda 
@@ -45,12 +43,11 @@ class LR:
     def __init__(self):
         return
 
-    # TODO Set bias remember
     def fit(self,x, y, max_epochs=100, threshold=0.01, learning_rate=0.001, momentum=0, decay=0, error = 'mse', regularization='none', lamda=0):
         # 1. Se pone los primeros atributos
         currentError = 0 # Para llevar el error
         maxError = sys.float_info.max # Para comparar en el threshold
-        C = np.random.random(x.shape[1])  # Para obtener los pesos iniciales de C
+        C = np.random.random(x.shape[1]+1)  # Para obtener los pesos iniciales de C
         prevDC = 0 # Para poder utilizar el valor previo de las derivada de C al obtener el momentum
         # 2. Se asignan las funciones de error y de la obtencion de los gradiantes
         errorFun = MAE if error == "mae" else MSE # Se elige la funcion de error
@@ -63,11 +60,13 @@ class LR:
             errorReg = ELASTIC if regularization == "elastic-net" else L1 if regularization =="l1" or regularization == "lasso" else L2
             regDer = ELASTICD if regularization == "elastic-net" else L1D if regularization =="l1" or regularization == "lasso" else L2D
         # 4. Se inicia el fit como tal
-        x = x.to_numpy()
+        x = np.insert(x.to_numpy(), 0, np.full(x.shape[0],1), axis=1)
+        y = y.to_numpy()
+
         for _ in itertools.repeat(None, max_epochs): # Se repite por n epocas
             y_predict = x.dot(C) # Se realiza la prediccion
             currentError= errorFun(y,y_predict) + errorReg(C,lamda)# Se calcula el error
-            if (maxError - currentError) < threshold: # Si no se supera el threshold, se finaliza el proceso del fit
+            if (currentError/maxError) < threshold: # Si no se supera el threshold, se finaliza el proceso del fit
                 break
             maxError = currentError # Se asigna el error nuevo maximo que se tiene
             dC = errorDer(x,y_predict,y) + regDer(C,lamda) # Se obtiene los valores de dC
@@ -78,4 +77,5 @@ class LR:
         self.C = C
 
     def predict(self, x):
+        x = np.insert(x.to_numpy(), 0, np.full(x.shape[0],1), axis=1)
         return x.dot(self.C)
